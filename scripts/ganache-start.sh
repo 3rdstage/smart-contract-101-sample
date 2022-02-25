1f22f896135ec43c3a06320c37070c6b58984d32
Sangmoon Oh <halfface@chollian.net>
2022-02-25 13:00:39 +0900

    Added a sample contract from CryptoPunks.

--- scripts/ganache-start.sh
+++ scripts/ganache-start.sh
@@ -5,10 +5,15 @@ ganache --chain.networkId 2016 \
   --chain.chainId 2016 \
   --server.host 127.0.0.1 \
   --server.port 8545 \
-  --miner.defaultGasPrice 25000000000 \
+  --miner.blockGasLimit 50000000000 \
   --miner.defaultTransactionGasLimit 400000000 \
   --miner.blockTime 0 \
+  --wallet.mnemonic "excess involve mom sick feel setup reflect elder voyage supreme evil flash" \
   --wallet.totalAccounts 15 \
   --wallet.defaultBalance 10000 \
-  --wallet.unlockedAccounts 0 1 2 3 4 \
+  --wallet.unlockedAccounts 0 \
+  --wallet.unlockedAccounts 1 \
+  --wallet.unlockedAccounts 2 \
+  --wallet.unlockedAccounts 3 \
+  --wallet.unlockedAccounts 4 \
   --database.dbPath run/ganache/data
